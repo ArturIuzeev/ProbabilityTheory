759ec9a94f498b17ed70b5b3f0c5c519db0d7961
Arthur <yuzeev.artur@mail.ru>
2023-05-16 17:15:20 +0300

    fix bugs

--- Lab1/Task4/main.py
+++ Lab1/Task4/main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 count = [10, 100, 1000, 10000]
 probabilities = [0.001, 0.01, 0.1, 0.25, 0.5]
+resCount = []
 exactProbabilities = []
 noExactProbabilities = []
 
@@ -30,7 +31,7 @@ def countFi2(x):
 def func1(n, p):
     q = 1 - p
     result = 0
-    for S in range(int(n / 2 - math.sqrt(n * p * q)), int(n / 2 + math.sqrt(n * p * q))):
+    for S in probabilities:
         c = math.sqrt(n * p * q)
         x = countX(S, c, n, p)
         fi = countFi(x)
@@ -59,15 +60,21 @@ def func2(n, p):
 
 for n in count:
     for p in probabilities:
-        func1(n, p)
-        func2(n, p)
-        print(n, p)
+        res1 = func1(n, p)
+        res2 = func2(n, p)
+        # print(n, p)
+        resCount.append(abs(res1 - res2))
+    print(resCount, n)
+    plt.plot(probabilities, resCount, label="n = {}".format(n))
+    resCount.clear()
 
-
-diff = [exactProbabilities[i] - noExactProbabilities[i] for i in range(len(exactProbabilities))]
-plt.plot(diff)
-plt.xlabel('Index')
-plt.ylabel('Difference')
-plt.title('Difference between Exact Probabilities and Approximations')
+plt.xlabel('Value probability')
+plt.ylabel('Value Differance')
+plt.title(' Difference between Exact Probabilities and Approximations')
 plt.show()
 
+# diff = [exactProbabilities[i] - noExactProbabilities[i] for i in range(len(exactProbabilities))]
+# # plt.plot(diff)
+# plt.plot(exactProbabilities, color="green")
+# plt.plot(noExactProbabilities, color="blue")
+# plt.show()
